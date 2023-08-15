69f624394756b4d27d27276a6522db87be4f47a8
Albert Devasia <albertdevasia2024@mca.ajce.in>
2023-08-15 15:26:50 +0530

    mod 4 v2

--- tutor1/tut04/logEvents.js
+++ tutor1/tut04/logEvents.js
@@ -1,10 +1,30 @@
-const {format} = require('date-fns');
-const {v4: uuid} = require('uuid');
+const { format } = require('date-fns');
+const { v4: uuid} = require('uuid');
 
 const fs = require('fs');
-const fsPromise = require('fs').promises;
+const fsPromises = require('fs').promises;
 const path = require('path');
 
-console.log(format(new Date(),'yyyy/MM/dd\tHH:mm:ss'));
+const logEvents =  async(message) => {
+    const dateTime = `${format(new Date(),'yyyy/MM/dd\tHH:mm:ss')}`
+    const logItem = `${dateTime}\t${uuid()}\t${message}`; 
+    console.log(logItem);
 
-console.log(uuid());
+    try{
+        if(!fs.existsSync(path.join(__dirname,'logs'))){
+            await fsPromises.mkdir(path.join(__dirname,'logs'))
+            
+        }
+        await fsPromises.appendFile(path.join(__dirname,'logs','eventLog.txt'),logItem)
+        
+    }catch(err){
+        console.log(err);
+
+    }
+
+}
+
+
+
+
+module.exports = logEvents;
